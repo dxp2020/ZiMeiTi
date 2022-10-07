5a08685e2c03b257d4b0ba314e766a23a1fef7c7
dxp2020 <364499854@qq.com>
2022-10-07 15:43:08 +0800

    dxp2022 盘点抗美援朝十大经典战役，不止上甘岭！

---
+++ app/src/main/java/com/example/bitmaptest/DanBingActivity.java
@@ -0,0 +1,358 @@
+package com.example.bitmaptest;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 朝鲜战场上的12位志愿军单兵之王，您知道几个？
+ */
+public class DanBingActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+    private Button btn_1;
+    private Button btn_2;
+    private Button btn_3;
+    private FrameLayout v_bitmap_view;
+    private Button btn_run;
+    private Button btn_read;
+    private Button btn_feng_mian;
+    private TextView tv_fengmian;
+    private List<String[]> list = new ArrayList<>();
+    private int index;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_danbing);
+
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+        v_bitmap_view = findViewById(R.id.v_bitmap_view);
+        btn_run = findViewById(R.id.btn_run);
+        btn_read = findViewById(R.id.btn_read);
+        btn_feng_mian = findViewById(R.id.btn_feng_mian);
+        tv_fengmian = findViewById(R.id.tv_fengmian);
+
+        findViewById(R.id.btn_0).setOnClickListener(view -> {
+            request();
+        });
+
+        btn_1.setOnClickListener(view -> {
+            handlerBtn1();
+        });
+        btn_2.setOnClickListener(view -> {
+            handlerBtn2();
+        });
+        btn_3.setOnClickListener(view -> {
+            handlerBtn3();
+        });
+        btn_feng_mian.setOnClickListener(view -> {
+            handlerBtn4();
+        });
+
+        btn_read.setOnClickListener(v -> {
+            btn_run.setEnabled(true);
+            index = 0;
+            list.clear();
+            readInfo();
+        });
+
+        btn_run.setOnClickListener(v -> {
+            handlePic(v);
+        });
+    }
+
+    private void handlerBtn4() {
+        v_bitmap_view.setVisibility(View.GONE);
+        tv_fengmian.setVisibility(View.VISIBLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000l);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = createBitmapFromView(tv_fengmian);
+
+            File file = new File("/mnt/sdcard/dxp2020/0.png");
+            try {
+                FileOutputStream out = new FileOutputStream(file);
+                if (bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)) {
+                    out.flush();
+                    out.close();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                handler.sendEmptyMessageDelayed(1, 500);
+            }
+        }).start();
+    }
+
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 0) {
+                index++;
+                if (index > list.size()) {
+                    return;
+                }
+                String[] data = list.get(index - 1);
+                TextView tv_num = v_bitmap_view.findViewById(R.id.tv_num);
+                TextView tv_chen_wei = v_bitmap_view.findViewById(R.id.tv_chen_wei);
+                TextView tv_name = v_bitmap_view.findViewById(R.id.tv_name);
+                TextView tv_address = v_bitmap_view.findViewById(R.id.tv_address);
+                TextView tv_desc = v_bitmap_view.findViewById(R.id.tv_desc);
+                ImageView iv_icon = v_bitmap_view.findViewById(R.id.iv_icon);
+
+                tv_num.setText(String.valueOf(index));
+                tv_chen_wei.setText(data[0]);
+                tv_name.setText(data[1]);
+                tv_desc.setText(data[2]);
+                tv_address.setText(data[3]);
+                iv_icon.setImageResource(getImageResource(index));
+
+
+                new Thread(() -> savePicture("/mnt/sdcard/dxp2020/" + index + ".png")).start();
+            } else if (msg.what == 1) {
+                tv_fengmian.setText("不朽中華魂!");
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(2000l);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap bitmap = createBitmapFromView(tv_fengmian);
+
+                    File file = new File("/mnt/sdcard/dxp2020/end.png");
+                    try {
+                        FileOutputStream out = new FileOutputStream(file);
+                        if (bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)) {
+                            out.flush();
+                            out.close();
+                        }
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        handler.sendEmptyMessageDelayed(2, 500);
+                    }
+                }).start();
+            } else if (msg.what == 2) {
+                v_bitmap_view.setVisibility(View.VISIBLE);
+                tv_fengmian.setVisibility(View.GONE);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        }
+    };
+
+    private int getImageResource(int id) {
+        String imgName = "a" + id;
+        return getResources().getIdentifier(imgName, "mipmap", getPackageName());
+    }
+
+    private void handlePic(View view) {
+        index = 0;
+        view.setEnabled(false);
+        handler.sendEmptyMessage(0);
+    }
+
+    public void readInfo() {
+        InputStream inputStream = null;
+        InputStreamReader isReader = null;
+        BufferedReader reader = null;
+        try {
+            //获取文件中的内容
+            inputStream = getResources().openRawResource(R.raw.version1);
+            //将文件中的字节转换为字符
+            isReader = new InputStreamReader(inputStream, "UTF-8");
+            //使用bufferReader去读取字符
+            reader = new BufferedReader(isReader);
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] arr = line.split("&");
+                list.add(arr);
+            }
+            Log.i(TAG, "list-->" + list.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                isReader.close();
+                inputStream.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    public void savePicture(String name) {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = createBitmapFromView(v_bitmap_view);
+
+        File file = new File(name);
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            if (bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)) {
+                out.flush();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            handler.sendEmptyMessageDelayed(0, 500);
+        }
+    }
+
+    public static Bitmap createBitmapFromView(View view) {
+        //是ImageView直接获取
+        if (view instanceof ImageView) {
+            Drawable drawable = ((ImageView) view).getDrawable();
+            if (drawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) drawable).getBitmap();
+            }
+        }
+        view.clearFocus();
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        if (bitmap != null) {
+            Canvas canvas = new Canvas(bitmap);
+            view.draw(canvas);
+            canvas.setBitmap(null);
+        }
+        return bitmap;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void request() {
+        String[] perms = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        for (String p : perms) {
+            int f = ContextCompat.checkSelfPermission(this, p);
+            Log.d("---", String.format("%s - %d", p, f));
+            if (f != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(perms, 0XCF);
+                break;
+            }
+        }
+    }
+
+    private void handlerBtn1() {
+        new Thread(() -> {
+            Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+            final int w = bitmap.getWidth();
+            final int h = bitmap.getHeight();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    bitmap.setPixel(i, j, Color.argb(0, 0, 0, 0));
+                }
+            }
+            saveFile(bitmap, "/mnt/sdcard/dxp2020/", "纯透明.png");
+        }).start();
+    }
+
+    private void handlerBtn2() {
+        new Thread(() -> {
+            Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+            final int w = bitmap.getWidth();
+            final int h = bitmap.getHeight();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    if ((i % 2 == 0 && j % 2 == 0) || ((i % 2 == 1 && j % 2 == 1))) {
+                        bitmap.setPixel(i, j, Color.argb(0xBF, 255, 255, 255));
+                    } else {
+                        bitmap.setPixel(i, j, Color.argb(0, 0, 0, 0));
+                    }
+                }
+            }
+            saveFile(bitmap, "/mnt/sdcard/dxp2020/", "相隔像素透明.png");
+        }).start();
+    }
+
+    public void handlerBtn3() {
+        new Thread(() -> {
+            Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+            final int w = bitmap.getWidth();
+            final int h = bitmap.getHeight();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    if ((i % 5 == 0 && j % 5 == 0)) {
+                        bitmap.setPixel(i, j, Color.argb(0xFF, 255, 255, 255));
+                    } else {
+                        bitmap.setPixel(i, j, Color.argb(0, 0, 0, 0));
+                    }
+                }
+            }
+            saveFile(bitmap, "/mnt/sdcard/dxp2020/", "相隔像素透明-加大.png");
+        }).start();
+    }
+
+    /**
+     * 将Bitmap转换成文件
+     */
+    public static File saveFile(Bitmap bm, String path, String fileName) {
+        File myCaptureFile = null;
+        try {
+            File dirFile = new File(path);
+            if (!dirFile.exists()) {
+                dirFile.mkdir();
+            }
+            myCaptureFile = new File(path, fileName);
+            myCaptureFile.createNewFile();
+
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(myCaptureFile));
+            bm.compress(Bitmap.CompressFormat.PNG, 100, bos);
+            bos.flush();
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myCaptureFile;
+    }
+
+}
---
+++ app/src/main/java/com/example/bitmaptest/ZhenWanActivity.java
@@ -0,0 +1,304 @@
+package com.example.bitmaptest;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 朝鮮战争各国阵亡人数
+ */
+public class ZhenWanActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+    private Button btn_1;
+    private Button btn_2;
+    private Button btn_3;
+    private FrameLayout v_bitmap_view;
+    private Button btn_run;
+    private Button btn_read;
+    private List<String[]> list = new ArrayList<>();
+    private int index;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_zhenwan);
+
+        btn_1 = findViewById(R.id.btn_1);
+        btn_2 = findViewById(R.id.btn_2);
+        btn_3 = findViewById(R.id.btn_3);
+        v_bitmap_view = findViewById(R.id.v_bitmap_view);
+        btn_run = findViewById(R.id.btn_run);
+        btn_read = findViewById(R.id.btn_read);
+
+        findViewById(R.id.btn_0).setOnClickListener(view -> {
+            request();
+        });
+
+        btn_1.setOnClickListener(view -> {
+            handlerBtn1();
+        });
+        btn_2.setOnClickListener(view -> {
+            handlerBtn2();
+        });
+        btn_3.setOnClickListener(view -> {
+            handlerBtn3();
+        });
+
+        btn_read.setOnClickListener(v -> {
+            btn_run.setEnabled(true);
+            index = 0;
+            list.clear();
+            readInfo();
+        });
+
+        btn_run.setOnClickListener(v -> {
+            handlePic(v);
+        });
+    }
+
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 0) {
+                index++;
+                if (index > list.size()) {
+                    return;
+                }
+                String[] data = list.get(index - 1);
+                ImageView iv_left = v_bitmap_view.findViewById(R.id.iv_left);
+                ImageView iv_right = v_bitmap_view.findViewById(R.id.iv_right);
+                ImageView iv_bottom = v_bitmap_view.findViewById(R.id.iv_bottom);
+                TextView tv_tag = v_bitmap_view.findViewById(R.id.tv_tag);
+                TextView tv_content = v_bitmap_view.findViewById(R.id.tv_content);
+
+                tv_tag.setText(data[1]);
+                StringBuffer sb = new StringBuffer();
+                sb.append("阵亡:" + data[2].substring(0, data[2].length() - 2));
+                if (!"受伤".equals(data[3])) {
+                    sb.append("\n受伤:" + data[3].substring(0, data[3].length() - 2));
+                }
+                if (!"战俘".equals(data[4])) {
+                    sb.append("\n战俘:" + data[4].substring(0, data[4].length() - 2));
+                }
+                if (!"失踪".equals(data[5])) {
+                    sb.append("\n失踪:" + data[5].substring(0, data[5].length() - 2));
+                }
+                tv_content.setText(sb.toString());
+                iv_left.setImageResource(getImageResourceFrom("aa" + index));
+                iv_right.setImageResource(getImageResourceFrom("bb" + index));
+                iv_bottom.setImageResource(getImageResourceFrom("cc" + index));
+
+                new Thread(() -> savePicture("/mnt/sdcard/dxp2020/" + index + ".png")).start();
+            }
+        }
+    };
+
+    private int getImageResource(int id) {
+        String imgName = "a" + id;
+        return getResources().getIdentifier(imgName, "mipmap", getPackageName());
+    }
+
+    private int getImageResourceFrom(String id) {
+        return getResources().getIdentifier(id, "mipmap", getPackageName());
+    }
+
+    private void handlePic(View view) {
+        index = 0;
+        view.setEnabled(false);
+        handler.sendEmptyMessage(0);
+    }
+
+    public void readInfo() {
+        InputStream inputStream = null;
+        InputStreamReader isReader = null;
+        BufferedReader reader = null;
+        try {
+            //获取文件中的内容
+            inputStream = getResources().openRawResource(R.raw.version1);
+            //将文件中的字节转换为字符
+            isReader = new InputStreamReader(inputStream, "UTF-8");
+            //使用bufferReader去读取字符
+            reader = new BufferedReader(isReader);
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] arr = line.split("&");
+                list.add(arr);
+            }
+            Log.i(TAG, "list-->" + list.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+                isReader.close();
+                inputStream.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+    public void savePicture(String name) {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Bitmap bitmap = createBitmapFromView(v_bitmap_view);
+
+        File file = new File(name);
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            if (bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)) {
+                out.flush();
+                out.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            handler.sendEmptyMessageDelayed(0, 500);
+        }
+    }
+
+    public static Bitmap createBitmapFromView(View view) {
+        //是ImageView直接获取
+        if (view instanceof ImageView) {
+            Drawable drawable = ((ImageView) view).getDrawable();
+            if (drawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) drawable).getBitmap();
+            }
+        }
+        view.clearFocus();
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        if (bitmap != null) {
+            Canvas canvas = new Canvas(bitmap);
+            view.draw(canvas);
+            canvas.setBitmap(null);
+        }
+        return bitmap;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void request() {
+        String[] perms = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        for (String p : perms) {
+            int f = ContextCompat.checkSelfPermission(this, p);
+            Log.d("---", String.format("%s - %d", p, f));
+            if (f != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(perms, 0XCF);
+                break;
+            }
+        }
+    }
+
+    private void handlerBtn1() {
+        new Thread(() -> {
+            Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+            final int w = bitmap.getWidth();
+            final int h = bitmap.getHeight();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    bitmap.setPixel(i, j, Color.argb(0, 0, 0, 0));
+                }
+            }
+            saveFile(bitmap, "/mnt/sdcard/dxp2020/", "纯透明.png");
+        }).start();
+    }
+
+    private void handlerBtn2() {
+        new Thread(() -> {
+            Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+            final int w = bitmap.getWidth();
+            final int h = bitmap.getHeight();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    if ((i % 2 == 0 && j % 2 == 0) || ((i % 2 == 1 && j % 2 == 1))) {
+                        bitmap.setPixel(i, j, Color.argb(0xBF, 255, 255, 255));
+                    } else {
+                        bitmap.setPixel(i, j, Color.argb(0, 0, 0, 0));
+                    }
+                }
+            }
+            saveFile(bitmap, "/mnt/sdcard/dxp2020/", "相隔像素透明.png");
+        }).start();
+    }
+
+    public void handlerBtn3() {
+        new Thread(() -> {
+            Bitmap bitmap = Bitmap.createBitmap(1920, 1080, Bitmap.Config.ARGB_8888);
+            final int w = bitmap.getWidth();
+            final int h = bitmap.getHeight();
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    if ((i % 5 == 0 && j % 5 == 0)) {
+                        bitmap.setPixel(i, j, Color.argb(0xFF, 255, 255, 255));
+                    } else {
+                        bitmap.setPixel(i, j, Color.argb(0, 0, 0, 0));
+                    }
+                }
+            }
+            saveFile(bitmap, "/mnt/sdcard/dxp2020/", "相隔像素透明-加大.png");
+        }).start();
+    }
+
+    /**
+     * 将Bitmap转换成文件
+     */
+    public static File saveFile(Bitmap bm, String path, String fileName) {
+        File myCaptureFile = null;
+        try {
+            File dirFile = new File(path);
+            if (!dirFile.exists()) {
+                dirFile.mkdir();
+            }
+            myCaptureFile = new File(path, fileName);
+            myCaptureFile.createNewFile();
+
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(myCaptureFile));
+            bm.compress(Bitmap.CompressFormat.PNG, 100, bos);
+            bos.flush();
+            bos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myCaptureFile;
+    }
+
+}
