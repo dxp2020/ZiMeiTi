f515a4cc6432b2b58b0c38a50ffd634fc6dc8037
dxp2020 <364499854@qq.com>
2022-10-10 22:47:06 +0800

    南京军区历任司令员，你认识几位？

--- app/src/main/java/com/example/bitmaptest/app/CommonActivity.java
+++ app/src/main/java/com/example/bitmaptest/app/CommonActivity.java
@@ -14,6 +14,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -39,42 +40,69 @@ public class CommonActivity extends Activity {
     private static final String TAG = "CommonActivity";
     private FrameLayout v_bitmap_view;
     private Button btn_run;
-    private Button btn_read;
     private Button btn_export;
+    private Button btn_switch;
     private TextView tv_fengmian;
     private List<String[]> list = new ArrayList<>();
     private int index;
+    private int templateIndex = -1;
+    private String title;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_yinxiong);
+        setContentView(R.layout.layout_common);
 
         v_bitmap_view = findViewById(R.id.v_bitmap_view);
         btn_run = findViewById(R.id.btn_run);
-        btn_read = findViewById(R.id.btn_read);
         btn_export = findViewById(R.id.btn_export);
         tv_fengmian = findViewById(R.id.tv_fengmian);
+        btn_switch = findViewById(R.id.btn_switch);
 
         findViewById(R.id.btn_0).setOnClickListener(view -> {
             request();
         });
-        
-        btn_export.setOnClickListener(view -> {
-            exportCover();
-        });
 
-        btn_read.setOnClickListener(v -> {
-            btn_run.setEnabled(true);
-            index = 0;
-            list.clear();
-            readInfo();
+        btn_export.setOnClickListener(view -> {
+            tv_fengmian.setText(title);
+            exportCover();
         });
 
         btn_run.setOnClickListener(v -> {
             handlePic(v);
         });
+
+        btn_switch.setOnClickListener(v -> {
+            btn_run.setEnabled(true);
+            index = 0;
+            list.clear();
+            readInfo();
+
+            templateIndex++;
+            templateIndex %= 4;
+            View view = null;
+            switch (templateIndex) {
+                case 0:
+                    view = View.inflate(CommonActivity.this, R.layout.layout_common_item_1, null);
+                    break;
+                case 1:
+                    view = View.inflate(CommonActivity.this, R.layout.layout_common_item_2, null);
+                    break;
+                case 2:
+                    view = View.inflate(CommonActivity.this, R.layout.layout_common_item_3, null);
+                    break;
+                case 3:
+                    view = View.inflate(CommonActivity.this, R.layout.layout_common_item_4, null);
+                    break;
+            }
+            if (view != null) {
+                v_bitmap_view.removeAllViews();
+                v_bitmap_view.addView(view);
+            }
+        });
+
+        new Handler().postDelayed(() -> btn_switch.performClick(), 500);
     }
 
     private void exportCover() {
@@ -117,17 +145,22 @@ public class CommonActivity extends Activity {
                 String[] data = list.get(index - 1);
                 TextView tv_num = v_bitmap_view.findViewById(R.id.tv_num);
                 TextView tv_chen_wei = v_bitmap_view.findViewById(R.id.tv_chen_wei);
-                TextView tv_name = v_bitmap_view.findViewById(R.id.tv_name);
-                TextView tv_address = v_bitmap_view.findViewById(R.id.tv_address);
-                TextView tv_desc = v_bitmap_view.findViewById(R.id.tv_desc);
+                TextView tv_line1 = v_bitmap_view.findViewById(R.id.tv_line1);
+                TextView tv_line2 = v_bitmap_view.findViewById(R.id.tv_line2);
+                TextView tv_line3 = v_bitmap_view.findViewById(R.id.tv_line3);
                 ImageView iv_icon = v_bitmap_view.findViewById(R.id.iv_icon);
 
                 iv_icon.setImageResource(getImageResource(data[0]));
                 tv_num.setText(String.valueOf(index));
-                tv_chen_wei.setText(data[3]);
-                tv_name.setText(data[1]);
-                tv_desc.setText(data[4]);
-                tv_address.setText(data[2]);
+                if (TextUtils.isEmpty(data[3])) {
+                    tv_chen_wei.setVisibility(View.GONE);
+                } else {
+                    tv_chen_wei.setVisibility(View.VISIBLE);
+                    tv_chen_wei.setText(data[3]);
+                }
+                tv_line1.setText(data[1]);
+                tv_line2.setText(data[2].replace("\\n", "\n"));
+                tv_line3.setText(data[4]);
 
 
                 new Thread(() -> savePicture("/mnt/sdcard/dxp2020/" + index + ".png")).start();
@@ -177,6 +210,7 @@ public class CommonActivity extends Activity {
         InputStream inputStream = null;
         InputStreamReader isReader = null;
         BufferedReader reader = null;
+        title = null;
         try {
             //获取文件中的内容
             inputStream = getResources().openRawResource(R.raw.version1);
@@ -186,8 +220,12 @@ public class CommonActivity extends Activity {
             reader = new BufferedReader(isReader);
             String line = "";
             while ((line = reader.readLine()) != null) {
-                String[] arr = line.split("&");
-                list.add(arr);
+                if (title == null) {
+                    title = line;
+                } else {
+                    String[] arr = line.split("&");
+                    list.add(arr);
+                }
             }
             Log.i(TAG, "list-->" + list.size());
         } catch (Exception e) {
--- app/src/main/java/com/example/bitmaptest/app/MakeMoneyApplication.java
+++ app/src/main/java/com/example/bitmaptest/app/MakeMoneyApplication.java
@@ -1,6 +1,10 @@
 package com.example.bitmaptest.app;
 
+import android.Manifest;
 import android.app.Application;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.example.bitmaptest.utils.FileUtil;
 
@@ -11,6 +15,18 @@ public class MakeMoneyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        String[] perms = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        for (String p : perms) {
+            int f = ContextCompat.checkSelfPermission(this, p);
+            if (f != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+
         File file = new File("/mnt/sdcard/dxp2020/");
         FileUtil.deleteFolder(file.getAbsolutePath());
         if (!file.exists()) {
